1eece8e61d522d48248bc02aefc94d987d4b4f98
Husnain0120 <muhammadhusnainrashid.bs@gmail.com>
2025-03-09 06:42:38 +0500

    feat: update middleware to handle token deletion on login redirect

--- opt_auth/src/middleware.js
+++ opt_auth/src/middleware.js
@@ -1,20 +1,23 @@
 import { NextResponse } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const path = request.nextUrl.pathname;
   const isPublicPath = path === "/login" || path === "/signup";
 
   const token = request.cookies.get("token")?.value || "";
+
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/", request.url));
   }
   if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    const response = NextResponse.redirect(new URL("/login", request.url));
+    response.cookies.delete("token"); // Ensure old cookies are removed
+    return response;
   }
+
+  return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ["/", "/login", "/signup"],
 };
